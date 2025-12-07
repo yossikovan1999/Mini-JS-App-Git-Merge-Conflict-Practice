467e0d36b298662daf2e53b98c89e42fe390118f
Yossi Kovan <yossikovan@gmail.com>
2025-12-07 09:23:00 +0200

    commiting the index.js file

---
+++ index.js
@@ -0,0 +1,8 @@
+import { THEME_COLOR } from "./config.js";
+
+function getHeroTitle() {
+  return "Welcome";
+}
+
+console.log("HERO TITLE:", getHeroTitle());
+console.log("THEME COLOR:", THEME_COLOR);
